07d2019a3023f3381ebe6a4cf81ce030e78563ab
TheConnMan <bcconn2112@gmail.com>
2015-11-15 15:01:52 -0500

    Add express server for web oauth
    A web server is required for Pocket OAuth redirects and authorization.

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 var config = require('./config');
 var pocket = require('pocket-api');
 var Q = require('q');
+var express = require('express');
+var app = express();
 
-Q.fcall(getRequestToken)
-.then(function(token) {
-	console.log(token);
+var pocketApiRoot = 'https://getpocket.com/auth/authorize?';
+
+app.get('/', function(req, res) {
+	constructRedirectUrl().then(function(url) {
+		res.send('<a href="' + url + '">Authorize</a>');
+	});
 });
+
+app.get('/response', function(req, res) {
+	res.send('Thanks');
+});
+
+app.listen(config.port);
+
+function constructRedirectUrl() {
+	return Q.fcall(getRequestToken)
+	.then(function() {
+		return pocketApiRoot + 'request_token=' + config.request_token + '&redirect_uri=' + config.url + ':' + config.port + '/response';
+	});
+}
 
 function getRequestToken() {
 	var d = Q.defer();
 	pocket.getRequestToken(config.api_key, function(data) {
-		d.resolve(data.code);
+		config.request_token = data.code;
+		d.resolve();
 	});
 	return d.promise;
 }
