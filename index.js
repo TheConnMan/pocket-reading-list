1c01e13ffacf3ab59478ceb0b5ab503048402655
TheConnMan <bcconn2112@gmail.com>
2015-11-15 15:26:01 -0500

    Add express sessions
    Added Express sessions so multiple users can be using the app at once.
    Also using the global config to stor information was just ugly.

--- index.js
+++ index.js
@@ -2,33 +2,56 @@ var config = require('./config');
 var pocket = require('pocket-api');
 var Q = require('q');
 var express = require('express');
-var app = express();
+var session = require('express-session');
 
+var app = express();
 var pocketApiRoot = 'https://getpocket.com/auth/authorize?';
 
+app.use(session({
+	secret: config.secret,
+	cookie: {
+		maxAge: 60000
+	}
+}));
+
 app.get('/', function(req, res) {
-	constructRedirectUrl().then(function(url) {
+	constructRedirectUrl(req.session).then(function(url) {
 		res.send('<a href="' + url + '">Authorize</a>');
 	});
 });
 
 app.get('/response', function(req, res) {
-	res.send('Thanks');
+	getAccessToken(req.session)
+	.then(function() {
+		res.send('Thanks');
+	});
 });
 
 app.listen(config.port);
 
-function constructRedirectUrl() {
-	return Q.fcall(getRequestToken)
+function constructRedirectUrl(session) {
+	return Q.fcall(function() {
+		return getRequestToken(session);
+	})
 	.then(function() {
-		return pocketApiRoot + 'request_token=' + config.request_token + '&redirect_uri=' + config.url + ':' + config.port + '/response';
+		return pocketApiRoot + 'request_token=' + session.request_token + '&redirect_uri=' + config.url + ':' + config.port + '/response';
 	});
 }
 
-function getRequestToken() {
+function getRequestToken(session) {
 	var d = Q.defer();
 	pocket.getRequestToken(config.api_key, function(data) {
-		config.request_token = data.code;
+		session.request_token = data.code;
+		d.resolve();
+	});
+	return d.promise;
+}
+
+function getAccessToken(session) {
+	var d = Q.defer();
+	pocket.getAccessToken(config.api_key, session.request_token, function(data) {
+		session.username = data.username;
+		session.access_token = data.access_token;
 		d.resolve();
 	});
 	return d.promise;
